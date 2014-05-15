399778d90b7848416b8aef0da5fc7f7f42725024
Marek Sudoł <m.sudol@smsapi.pl>
2014-05-15 14:25:11 +0200

    add basic support for mms

---
+++ example_mms.d
@@ -0,0 +1,33 @@
+#!/usr/bin/env rdmd
+
+import SMSAPILib : Api, Mms, Receiver, Smil, Subject, User;
+import std.stdio : writeln;
+
+void main(string[] args)
+{
+    writeln(
+        (new Api(User("username", "password"), true))
+            .send(
+                new Mms(
+                    Subject("Test"),
+                    Receiver(790216004),
+                    Smil(
+                        `<smil>
+                            <head>
+                                <layout>
+                                    <root-layout height="600" width="425"/>
+                                    <region id="img" top="0" left="0" height="100%" width="100%" fit="meet" />
+                                </layout>
+                            </head>
+                            <body>
+                                <par dur="5000ms">
+                                    <img src="http://www.smsapi.pl/media/mms.jpg" region="img" />
+                                </par>
+                            </body>
+                        </smil>`
+                    )
+                )
+            )
+            .content
+    );
+}
